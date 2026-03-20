68c389d3e8953e34f171eddb23187fe1e7491575
yohany99 <yohany99@berkeley.edu>
2026-03-19 19:07:35 -0700

    Implement copy_static function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from textnode import TextType, TextNode
+from copystatic import copy_static
 
 def main():
-    test_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(test_node)
+    copy_static("static", "public")
 
 if __name__ == "__main__":
     main()
